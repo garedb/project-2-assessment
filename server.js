b8fa400c35ea5e4acea735908960d142fe6a8d77
garedb <garedbarney@gmail.com>
2020-05-01 17:14:54 -0700

    done

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const methodOverride = require('method-override');
+const db = require('./models')
 
 const app = express();
 
@@ -9,7 +10,31 @@ app.use(express.static('static'));
 app.use(methodOverride('_method'));
 
 // WRITE YOUR ROUTES HERE /////////////////////
+app.get('/', (req, res) => {
+	db.widget.findAll()
+	.then(widget => {
+		res.render('index', { widget: widget })	
+	})
+})
 
+app.post('/', (req, res) => {
+    db.widget.create({
+    	description: req.body.description,
+    	quantity: req.body.quantity,
+	})
+    .then((post) => {
+        res.redirect('/')
+    })
+})
+
+app.delete('/:id', (req, res) => {
+    db.widget.destroy({
+        where: { id: req.params.id}
+    })
+    .then(() => {
+        res.redirect('/')
+	})
+})
 
 
 // YOUR ROUTES ABOVE THIS COMMENT /////////////
